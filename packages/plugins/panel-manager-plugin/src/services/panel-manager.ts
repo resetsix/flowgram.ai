498454d86492a1c91f55e6c4bc2f47ded79548df
squall <squall_lau@qq.com>
2025-12-08 14:30:38 +0800

    feat: Panel support keepDOM (#1007)
    * feat: Panel support keepDOM
    
    * chore: cleanup code

--- packages/plugins/panel-manager-plugin/src/services/panel-manager.ts
+++ packages/plugins/panel-manager-plugin/src/services/panel-manager.ts
@@ -42,7 +42,12 @@ export class PanelManager {
 
     const sameKeyPanels = this.getPanels(area).filter((p) => p.key === key);
     if (!factory.allowDuplicates && sameKeyPanels.length) {
-      sameKeyPanels.forEach((p) => this.remove(p.id));
+      !factory.keepDOM && sameKeyPanels.forEach((p) => this.remove(p.id));
+    }
+
+    if (factory.keepDOM && sameKeyPanels.length) {
+      sameKeyPanels[0].visible = true;
+      return;
     }
 
     const panel = this.createPanel({
@@ -62,12 +67,19 @@ export class PanelManager {
   public close(key?: string) {
     const panels = this.getPanels();
     const closedPanels = key ? panels.filter((p) => p.key === key) : panels;
-    closedPanels.forEach((p) => this.remove(p.id));
+    closedPanels.forEach((panel) => {
+      if (panel.keepDOM) {
+        panel.visible = false;
+        return;
+      }
+
+      this.remove(panel.id)
+    });
     this.onPanelsChangeEvent.fire();
   }
 
   private trim(area: Area) {
-    const panels = this.getPanels(area);
+    const panels = this.getPanels(area).filter((p) => !p.keepDOM);
     const areaConfig = this.getAreaConfig(area);
     while (panels.length > areaConfig.max) {
       const removed = panels.shift();
