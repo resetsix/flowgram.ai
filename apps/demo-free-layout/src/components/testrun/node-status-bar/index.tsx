a7f36767aae9686f771e4489acb22542c706295b
lq9958 <34290782+lq9958@users.noreply.github.com>
2025-12-09 18:49:20 +0800

    fix(demo): node report status not updating (#1015)

--- apps/demo-free-layout/src/components/testrun/node-status-bar/index.tsx
+++ apps/demo-free-layout/src/components/testrun/node-status-bar/index.tsx
@@ -3,6 +3,11 @@
  * SPDX-License-Identifier: MIT
  */
 
+/**
+ * Copyright (c) 2025 Bytedance Ltd. and/or its affiliates
+ * SPDX-License-Identifier: MIT
+ */
+
 import { useEffect, useState } from 'react';
 
 import { NodeReport } from '@flowgram.ai/runtime-interface';
@@ -22,7 +27,10 @@ const useNodeReport = () => {
       if (nodeReport.id !== node.id) {
         return;
       }
-      setReport(nodeReport);
+      setReport((prev) =>({
+        ...prev,
+        ...nodeReport,
+      }));
     });
     const resetDisposer = runtimeService.onReset(() => {
       setReport(undefined);
