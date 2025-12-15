59ef0d3240d554204702079f36966a1eee8648e9
Louis Young <63398145+louisyoungx@users.noreply.github.com>
2025-12-15 20:48:13 +0800

    feat: sub canvas demo (#1028)
    * feat: add sub-canvas demo application
    
    * feat: add block start/end and loop nodes with subcanvas support
    
    * feat: add batch node with automatic batch-function subcanvas creation

---
+++ apps/demo-sub-canvas/src/nodes/batch-function/index.ts
@@ -0,0 +1,11 @@
+/**
+ * Copyright (c) 2025 Bytedance Ltd. and/or its affiliates
+ * SPDX-License-Identifier: MIT
+ */
+
+export { createBatchFunctionJSON } from './create-batch-function-json';
+export { createBatchFunctionLines } from './create-batch-function-lines';
+export { createBatchFunction } from './create-batch-function';
+export { BatchFunctionFormRender, formMeta } from './form-meta';
+export { BatchFunctionNodeRegistry } from './registry';
+export { BatchFunctionIDPrefix, getBatchFunctionID, getBatchID } from './relation';
---
+++ apps/demo-sub-canvas/src/nodes/block-start/index.ts
@@ -0,0 +1,41 @@
+/**
+ * Copyright (c) 2025 Bytedance Ltd. and/or its affiliates
+ * SPDX-License-Identifier: MIT
+ */
+
+import { FlowNodeRegistry } from '@flowgram.ai/free-layout-editor';
+
+import { formMeta } from './form-meta';
+
+export const BlockStartNodeRegistry: FlowNodeRegistry = {
+  type: 'block_start',
+  meta: {
+    isStart: true,
+    deleteDisable: true,
+    copyDisable: true,
+    sidebarDisabled: true,
+    nodePanelVisible: false,
+    defaultPorts: [{ type: 'output' }],
+    size: {
+      width: 100,
+      height: 100,
+    },
+    wrapperStyle: {
+      minWidth: 'unset',
+      width: '100%',
+      borderWidth: 2,
+      borderRadius: '50%',
+      cursor: 'move',
+    },
+  },
+  /**
+   * Render node via formMeta
+   */
+  formMeta,
+  /**
+   * Start Node cannot be added
+   */
+  canAdd() {
+    return false;
+  },
+};
